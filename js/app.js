e7c1379ff05aa8d618ee3ac3c1c2cf833fe1cfbf
Christian Krzekotowski da Cunha <christian_cunha@terceiros.sicredi.com.br>
2018-12-04 13:28:47 -0200

    Fix shuffle and some behaviors.

--- js/app.js
+++ js/app.js
@@ -1,36 +1,50 @@
-debugger;
 let card = document.getElementsByClassName("card");
-let cards = [...card];
-let deck
+const cards = [...card];
+const deck = document.getElementById("card-deck");
 let list = [];
 let matchNum = 0;
 let startTime = new Date().getTime();
 let endTime = new Date().getTime();
+let matchPointsCounter = 0;
+let missPointsCounter = 0;
+let removedStars = 0;
 init();
 
 function init() {
-    $(".open").removeClass("open");
-    $(".show").removeClass("show");
-    $(".match").removeClass("match");
     $('.moves').text(0);
     $('.stars').html('<li><i class="fa fa-star"></i></li> <li><i class="fa fa-star"></i></li> <li><i class="fa fa-star"></i></li>');
-    cards = shuffle(cards);
+    prepareBoard();
     matchNum = 0;
     startTime = new Date().getTime();
     endTime = new Date().getTime();
 }
 
+function prepareBoard() {
+    let shuffleCards = shuffle(cards);
+    for (var i = 0; i < shuffleCards.length; i++) {
+        deck.innerHTML = "";
+        [].forEach.call(cards, function(item){
+            deck.appendChild(item);
+        });
+        cards[i].classList.remove("show", "open", "match");
+    }
+}
+
 function match() {
     $('li').find('open').addClass('match');
+    matchPointsCounter += 10;
 }
 
 function addCounter() {
     let countNum = Number($('.moves').text());
-    countNum += 1;
+    countNum++;
     $('.moves').text(countNum);
 
-    if (countNum % 2 === 0) {
-        $(".stars").find('li:last-child').remove();
+    if (countNum % 5 === 0) {
+        if (removedStars < 2) {
+            $(".stars").find('li:last-child').remove();
+            removedStars++;
+        }
     }
 }
 
@@ -41,8 +55,8 @@ function again() {
     });
 }
 
-function score(moves, yourTime) {
-    return (Math.round(Math.max(yourTime / Number(moves), 0)));
+function score() {
+    return Math.max(matchPointsCounter - missPointsCounter, 0);
 }
 
 function verifier() {
@@ -54,6 +68,7 @@ function verifier() {
             match();
             matchNum += 1;
         } else {
+            missPointsCounter += 2;
             addCounter();
         }
     }
@@ -61,11 +76,12 @@ function verifier() {
         const endTime = new Date().getTime();
         const yourTime = ((endTime - startTime) / 1000);
         let moves = $(".moves").text();
+        let scoreStars = 3 - removedStars;
 
         $("#background").fadeIn(500);
         $("#notification-box").fadeIn(500);
         again();
-        $("#score").text(`Your score is:  ${score(moves, yourTime)}`);
+        $("#score").text(`Your score is:  ${score()} with ${scoreStars} ${scoreStars === 1 ? "star" : "stars"}`);
         $("#time").text(`Your time is: ${yourTime.toFixed(2)} s`);
     }
 }
@@ -98,16 +114,18 @@ $('.restart').click(function () {
 
 $('.card').click(function () {
     if (list.length === 2) {
-        $(".open, .show").removeClass("open show");
         list.pop();
         list.pop();
+        closeCards();
     }
-    if ($(this).hasClass("open")) {
-
-    } else {
+    if (!$(this).hasClass("open")) {
         $(this).addClass("open");
         $(this).addClass("show");
         addToList($(this).find('i').attr('class'));
         verifier();
     }
 });
+
+function closeCards() {
+    $(".open, .show").removeClass("open show");
+}
