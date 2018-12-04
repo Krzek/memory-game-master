9ff602125a013adfd3a846a6bb17fd694f6eac4e
Christian Krzekotowski da Cunha <krzek.cunha@gmail.com>
2018-12-04 01:20:05 -0200

    Creating the MVP.

--- js/app.js
+++ js/app.js
@@ -1,18 +1,82 @@
-/*
- * Create a list that holds all of your cards
- */
+debugger;
+let card = document.getElementsByClassName("card");
+let cards = [...card];
+let deck
+let list = [];
+let matchNum = 0;
+let startTime = new Date().getTime();
+let endTime = new Date().getTime();
+init();
 
+function init() {
+    $(".open").removeClass("open");
+    $(".show").removeClass("show");
+    $(".match").removeClass("match");
+    $('.moves').text(0);
+    $('.stars').html('<li><i class="fa fa-star"></i></li> <li><i class="fa fa-star"></i></li> <li><i class="fa fa-star"></i></li>');
+    cards = shuffle(cards);
+    matchNum = 0;
+    startTime = new Date().getTime();
+    endTime = new Date().getTime();
+}
 
-/*
- * Display the cards on the page
- *   - shuffle the list of cards using the provided "shuffle" method below
- *   - loop through each card and create its HTML
- *   - add each card's HTML to the page
- */
+function match() {
+    $('li').find('open').addClass('match');
+}
+
+function addCounter() {
+    let countNum = Number($('.moves').text());
+    countNum += 1;
+    $('.moves').text(countNum);
+
+    if (countNum % 2 === 0) {
+        $(".stars").find('li:last-child').remove();
+    }
+}
+
+function again() {
+    $('#play-again').click(function () {
+        $('#background').fadeOut(300);
+        init();
+    });
+}
+
+function score(moves, yourTime) {
+    return (Math.round(Math.max(yourTime / Number(moves), 0)));
+}
+
+function verifier() {
+    if (list.length === 2) {
+        if (list[0] === list[1]) {
+            list.pop();
+            list.pop();
+            $(".open, .show").addClass("match");
+            match();
+            matchNum += 1;
+        } else {
+            addCounter();
+        }
+    }
+    if (matchNum === 8) {
+        const endTime = new Date().getTime();
+        const yourTime = ((endTime - startTime) / 1000);
+        let moves = $(".moves").text();
+
+        $("#background").fadeIn(500);
+        $("#notification-box").fadeIn(500);
+        again();
+        $("#score").text(`Your score is:  ${score(moves, yourTime)}`);
+        $("#time").text(`Your time is: ${yourTime.toFixed(2)} s`);
+    }
+}
+
+function addToList(kind) {
+    list.push(kind);
+}
 
 // Shuffle function from http://stackoverflow.com/a/2450976
 function shuffle(array) {
-    var currentIndex = array.length, temporaryValue, randomIndex;
+    let currentIndex = array.length, temporaryValue, randomIndex;
 
     while (currentIndex !== 0) {
         randomIndex = Math.floor(Math.random() * currentIndex);
@@ -25,14 +89,25 @@ function shuffle(array) {
     return array;
 }
 
+$('#notification-box').hide();
+$('#background').hide();
 
-/*
- * set up the event listener for a card. If a card is clicked:
- *  - display the card's symbol (put this functionality in another function that you call from this one)
- *  - add the card to a *list* of "open" cards (put this functionality in another function that you call from this one)
- *  - if the list already has another card, check to see if the two cards match
- *    + if the cards do match, lock the cards in the open position (put this functionality in another function that you call from this one)
- *    + if the cards do not match, remove the cards from the list and hide the card's symbol (put this functionality in another function that you call from this one)
- *    + increment the move counter and display it on the page (put this functionality in another function that you call from this one)
- *    + if all cards have matched, display a message with the final score (put this functionality in another function that you call from this one)
- */
+$('.restart').click(function () {
+    init();
+})
+
+$('.card').click(function () {
+    if (list.length === 2) {
+        $(".open, .show").removeClass("open show");
+        list.pop();
+        list.pop();
+    }
+    if ($(this).hasClass("open")) {
+
+    } else {
+        $(this).addClass("open");
+        $(this).addClass("show");
+        addToList($(this).find('i').attr('class'));
+        verifier();
+    }
+});
